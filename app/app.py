c3519377c470ef4f442940f13796c098dfc3c782
amir <amirsmvb@yahoo.com>
2022-05-28 12:51:15 +0430

    requirements updated

---
+++ app/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+db = SQLAlchemy()
+
+
+@app.route('/')
+def index():
+	return "Hello, World"
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
